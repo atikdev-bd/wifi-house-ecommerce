44563374fa9e15f767ffa1b16419f0b36ca2c4fb
atikdev-bd <atikdev.bd@gmail.com>
2022-11-08 00:13:21 +0600

    navber footer and folder structure

---
+++ src/Context/AuthProvider/AuthProvider.js
@@ -0,0 +1,18 @@
+import React, { createContext } from 'react';
+
+
+
+ export const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+
+    const info ={}
+
+    return (
+        <AuthContext.Provider value={info}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+      </div>
+      <div className="mr-28">
+        <Link className="mr-3">Home</Link>
+        <Link className="mr-3">Add Service</Link>
+        <Link className="mr-3">Blogs</Link>
+        <Link className="mr-3">My Review</Link>
+      </div>
+
+      <div className="flex-none gap-2">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" alt="" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </Link>
+            </li>
+            <li>
+              <Link>Settings</Link>
+            </li>
+            <li>
+              <Link>Logout</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
